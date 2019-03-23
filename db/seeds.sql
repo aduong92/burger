858543361d991da80ea9f887e38db1106789b7f4
aduong92 <44982934+aduong92@users.noreply.github.com>
2019-03-23 04:43:12 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Drunk", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Nugget Special", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Southern Sun", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Eastern Heat", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avalanche", true);
